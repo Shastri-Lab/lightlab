83459c0baad1c4309570fb483ef8398d68750dbd
hughmor <hugh.morison@hotmail.ca>
2023-09-19 19:58:39 -0400

    fixed permissions error

--- lightlab/equipment/lab_instruments/itla_msa_modules/install_service.sh
+++ lightlab/equipment/lab_instruments/itla_msa_modules/install_service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp ~/lightlab/lightlab/equipment/lab_instruments/itla_msa_modules/microITLA_server.service /etc/systemd/system/microITLA_server.service
+cp $(eval echo ~$SUDO_USER)/lightlab/lightlab/equipment/lab_instruments/itla_msa_modules/microITLA_server.service /etc/systemd/system/microITLA_server.service
 sudo systemctl daemon-reload
 sudo systemctl enable microITLA_server
 sudo systemctl start microITLA_server
