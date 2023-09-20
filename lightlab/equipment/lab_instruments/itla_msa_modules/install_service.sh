05259418fe9ee5e65156166f50f5802c861dcc89
hughmor <hugh.morison@hotmail.ca>
2023-09-19 20:34:46 -0400

    more sophisticated install script

--- lightlab/equipment/lab_instruments/itla_msa_modules/install_service.sh
+++ lightlab/equipment/lab_instruments/itla_msa_modules/install_service.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-cp $(eval echo ~$SUDO_USER)/lightlab/lightlab/equipment/lab_instruments/itla_msa_modules/microITLA_server.service /etc/systemd/system/microITLA_server.service
+
+# Set variables for service template
+USERNAME=$(whoami)
+GROUPNAME=$(id -gn)
+USER_HOME=$(eval echo ~$USERNAME)
+
+# Substitute variables in template service and install
+sed "s|%USERNAME%|$USERNAME|g; s|%GROUPNAME%|$GROUPNAME|g; s|%USER_HOME%|$USER_HOME|g" microITLA_server.service.template > microITLA_server.service
+sudo mv microITLA_server.service /etc/systemd/system/microITLA_server.service
+
+# Reload systemd and enable service
 sudo systemctl daemon-reload
 sudo systemctl enable microITLA_server
 sudo systemctl start microITLA_server
